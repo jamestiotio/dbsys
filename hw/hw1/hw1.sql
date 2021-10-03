27521b183626cd644b89478714348b1b09793c6d
James R T <jamestiotio@gmail.com>
2021-10-03 18:45:36 +0800

    Add initial homework assignment 1 files

---
+++ hw/hw1/hw1.sql
@@ -0,0 +1,28 @@
+-- STUDENT number. UPDATE ONLY, DO NOT DELETE. 
+select "1004555";
+-- Replace the above with your student number
+
+-- DATA LOADING Seperator. DO NOT DELETE
+select "DATA LOADING";
+-- Put your table creation annd data loading SQL statements here
+
+
+-- QUESTION 1 Seperator. DO NOT DELETE
+select "QUESTION 1";
+-- Put your Q1 SQL statements here
+
+-- QUESTION 2 Seperator. DO NOT DELETE
+select "QUESTION 2";
+-- Put your Q2 SQL statements here
+
+-- QUESTION 3 Seperator. DO NOT DELETE
+select "QUESTION 3";
+-- Put your Q3 SQL statements here
+
+-- QUESTION 4 Seperator. DO NOT DELETE
+select "QUESTION 4";
+-- Put your Q4 SQL statements here
+
+-- QUESTION 5 Seperator. DO NOT DELETE
+select "QUESTION 5";
+-- Put your Q5 SQL statements here
